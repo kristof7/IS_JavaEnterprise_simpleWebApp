2e72bd9a1c1f3b19fa1c04f3afdbeb17989e830c
Rafal Misiak <rafalmisiak@gmail.com>
2020-04-04 13:46:24 +0200

    Add user entity class

---
+++ usersengine/src/main/java/com/isa/usersengine/domain/User.java
@@ -0,0 +1,54 @@
+package com.isa.usersengine.domain;
+
+public class User {
+
+  private Long id;
+
+  private String name;
+
+  private String login;
+
+  private String password;
+
+  private Integer age;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getLogin() {
+    return login;
+  }
+
+  public void setLogin(String login) {
+    this.login = login;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public Integer getAge() {
+    return age;
+  }
+
+  public void setAge(Integer age) {
+    this.age = age;
+  }
+}
